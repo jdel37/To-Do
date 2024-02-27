d529ef3e493e74ca6423d4248de751b2f4703058
unknown <juanforero@cca.edu.co>
2024-02-26 21:36:03 -0500

    tailwind form

--- To-Do-List/src/App.jsx
+++ To-Do-List/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
@@ -8,10 +7,11 @@ function App() {
 
   return (
     
+     <form action="" className='flex content-center'>
+      <input type="text" className=' content-center w-96 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500' />
+      <button className='content-center bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>submit</button>
+     </form>
      
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
   )
 }
 
